897d1780aca7bb4730321a23388d9cb59ded79c7
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-03-28 19:08:46 +0000

    completed functions section

--- functions.js
+++ functions.js
@@ -1,1 +1,32 @@
-// functions below
+// Display todos
+
+function displayTodos() {
+    console.log('My todos:', todos);
+}
+
+displayTodos();
+
+// Add todos
+
+function addTodo(todo) {
+	todos.push(todo);
+	displayTodos();
+}
+
+// Change todos
+
+function changeTodo(position, newValue) {
+	todos[position] = newValue;
+	displayTodos();
+}
+
+changeTodo(0, 'changed again')
+
+// Delete todos
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+deleteTodo(0)
